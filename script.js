ff7345005df935f17e984b8e40750f81b5852f8b
incerahmaniaamirniar-creator <incerahmaniaamirniar@gmail.com>
2026-03-03 15:17:14 +0700

    Menambahkan java.script

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// Scroll ke section
-function scrollToSection(id) {
-    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+function scrollToSection(id){
+    document.getElementById(id).scrollIntoView({behavior:"smooth"});
 }
 
-// Animasi muncul saat discroll
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
+const observer=new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
             entry.target.classList.add("show");
         }
     });
 });
 
-document.querySelectorAll(".hidden").forEach((el) => observer.observe(el));
+document.querySelectorAll(".hidden").forEach(el=>{
+    observer.observe(el);
+});
